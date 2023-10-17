ef861cf7f2dd4d10dd72a8234c151d6ecf249d50
manit77 <manit@chanthavong.com>
2023-10-17 09:34:10 -0500

    add hello and env port

--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,16 @@ const path = require('path');
   expressApp.options('*', cors()) // include before other routes  
   expressApp.use(express.static(path.join(__dirname, "../public")));
 
+  expressApp.get("/", async function (req, res, next) {
+    res.write("hello from node-web-server");
+    res.end();
+  });
+
   //start the http server
-  let port = 80;
+  let port = "80";
+  if(process.env.WEBSERVERPORT){
+    port = process.env.WEBSERVERPORT;
+  }
   httpServer.listen(port, () => {
     console.log(`http://localhost:${port}`);
   });
